c95973d6652984641ec2a8fe88dfdd4284352a4a
Karl Rohr <kjrohr@fullsail.edu>
2016-11-03 19:54:20 -0400

    Fixed title, working off of a tutorial and then making our own

--- read_data.php
+++ read_data.php
@@ -5,7 +5,7 @@ include_once 'dbconfig.php';
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>CRUD Operations With PHP and MySql - By Cleartuts</title>
+<title>Admin Dashboard</title>
 <link rel="stylesheet" href="style.css" type="text/css" />
 </head>
 <body>
