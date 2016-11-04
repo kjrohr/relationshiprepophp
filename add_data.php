d9d4283b36b8f6ed75c21070f3506122a20d8d78
Lindsay Hampton <lrhampton@fullsail.edu>
2016-11-03 22:13:41 -0400

    More bootstrap fun on the sign up

--- add_data.php
+++ add_data.php
@@ -37,38 +37,37 @@ if(isset($_POST['btn-save']))
  <div id="content">
     <form method="post">
       <a href="index.php">back to main page</a></td>
-      
+
       <div class="container">
                   <form class="form-horizontal" role="form">
                       <h2>Registration Form</h2>
                       <div class="form-group">
                           <label for="username" class="col-sm-3 control-label">Username </label>
-                          <div class="col-sm-9">
+                          <div class="col-sm-6">
                               <input type="text" name="username" placeholder="Username" class="form-control" required />
-                              <span class="help-block">Last Name, First Name, eg.: Smith, Harry</span>
                           </div>
                       </div>
                       <div class="form-group">
                           <label for="first_name" class="col-sm-3 control-label">First Name</label>
-                          <div class="col-sm-9">
+                          <div class="col-sm-6">
                               <input type="text" name="first_name" placeholder="First Name" class="form-control" required />
                           </div>
                       </div>
                       <div class="form-group">
                           <label for="last_name" class="col-sm-3 control-label">Last Name</label>
-                          <div class="col-sm-9">
+                          <div class="col-sm-6">
                               <input type="text" name="last_name" placeholder="Last Name" class="form-control" required />
                           </div>
                       </div>
                       <div class="form-group">
                           <label for="email" class="col-sm-3 control-label">Email</label>
-                          <div class="col-sm-9">
+                          <div class="col-sm-6">
                               <input type="email" name="email" placeholder="example@example.com" class="form-control" required />
                           </div>
                       </div>
                       <div class="form-group">
                           <label for="address" class="col-sm-3 control-label">Country</label>
-                          <div class="col-sm-9">
+                          <div class="col-sm-6">
                               <input type="text" name="address" placeholder="123 Test Lane Winter Park FL, 32792" class="form-control" required />
                           </div>
                       </div> <!-- /.form-group -->
@@ -79,16 +78,16 @@ if(isset($_POST['btn-save']))
                           </div>
                       </div> <!-- /.form-group -->
                       <div class="form-group">
-                          <div class="col-sm-9 col-sm-offset-3">
+                          <div class="col-sm-6 col-sm-offset-3">
                               <div class="checkbox">
                                   <label>
-                                      <input type="checkbox">I accept <a href="#">terms</a>
+                                      <input type="checkbox">By checking this box, I hearby accept Relationship Repo's <a href="#">Terms and Conditions</a>.
                                   </label>
                               </div>
                           </div>
                       </div> <!-- /.form-group -->
                       <div class="form-group">
-                          <div class="col-sm-9 col-sm-offset-3">
+                          <div class="col-sm-6 col-sm-offset-3">
                               <button type="submit" name="btn-save" class="btn btn-primary btn-block">Register</button>
                           </div>
                       </div>
