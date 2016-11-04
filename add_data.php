64524c31b7680a51358e6008feb702c7be94956f
Lindsay Hampton <lrhampton@fullsail.edu>
2016-11-03 22:31:53 -0400

    CSS fix on container

--- add_data.php
+++ add_data.php
@@ -42,43 +42,37 @@ if(isset($_POST['btn-save']))
                   <form class="form-horizontal" role="form">
                       <h2>Registration Form</h2>
                       <div class="form-group">
-                          <label for="username" class="col-sm-3 control-label">Username </label>
-                          <div class="col-sm-7">
+                        <div class="col-sm-7">
                               <input type="text" name="username" placeholder="Username" class="form-control" required />
                           </div>
                       </div>
                       <div class="form-group">
-                          <label for="first_name" class="col-sm-3 control-label">First Name</label>
                           <div class="col-sm-7">
                               <input type="text" name="first_name" placeholder="First Name" class="form-control" required />
                           </div>
                       </div>
                       <div class="form-group">
-                          <label for="last_name" class="col-sm-3 control-label">Last Name</label>
-                          <div class="col-sm-7">
+                        <div class="col-sm-7">
                               <input type="text" name="last_name" placeholder="Last Name" class="form-control" required />
                           </div>
                       </div>
                       <div class="form-group">
-                          <label for="email" class="col-sm-3 control-label">Email</label>
-                          <div class="col-sm-7">
-                              <input type="email" name="email" placeholder="example@example.com" class="form-control" required />
+                        <div class="col-sm-7">
+                              <input type="email" name="email" placeholder="Email Address" class="form-control" required />
                           </div>
                       </div>
                       <div class="form-group">
-                          <label for="address" class="col-sm-3 control-label">Country</label>
                           <div class="col-sm-7">
-                              <input type="text" name="address" placeholder="123 Test Lane Winter Park FL, 32792" class="form-control" required />
+                              <input type="text" name="address" placeholder="Address, City, State, Zip" class="form-control" required />
                           </div>
                       </div> <!-- /.form-group -->
                       <div class="form-group">
-                          <label for="age" class="col-sm-3 control-label">Age</label>
-                          <div class="col-sm-7">
-                          <input type="number" name="age" placeholder="22" class="form-control" required />
+                        <div class="col-sm-7">
+                          <input type="number" name="age" placeholder="Your Age" class="form-control" required />
                           </div>
                       </div> <!-- /.form-group -->
                       <div class="form-group">
-                          <div class="col-sm-9 col-sm-offset-3">
+                          <div class="col-sm-9">
                               <div class="checkbox">
                                   <label>
                                       <input type="checkbox">By checking this box, I hearby accept Relationship Repo's <a href="#">Terms and Conditions</a>.
@@ -87,7 +81,7 @@ if(isset($_POST['btn-save']))
                           </div>
                       </div> <!-- /.form-group -->
                       <div class="form-group">
-                          <div class="col-sm-9 col-sm-offset-3">
+                          <div class="col-sm-9">
                               <button type="submit" name="btn-save" class="btn btn-primary btn-block">Register</button>
                           </div>
                       </div>
