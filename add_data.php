c39e47eac4372a548b124032555dcdf89c2ba7b9
Karl Rohr <kjrohr@fullsail.edu>
2016-11-03 21:11:17 -0400

    git hook test

--- add_data.php
+++ add_data.php
@@ -3,17 +3,13 @@ include_once 'dbconfig.php';
 if(isset($_POST['btn-save']))
 {
  // variables for input data
- $username = $_POST['username'];
  $first_name = $_POST['first_name'];
  $last_name = $_POST['last_name'];
- $age = $_POST['age'];
- $email = $_POST['email'];
- $address = $_POST['address'];
  // variables for input data
 
  // sql query for inserting data into database
 
-        $sql_query = "INSERT INTO users (username,first_name,last_name,age,email,address) VALUES('$username',$first_name','$last_name','$age','$email','$address')";
+        $sql_query = "INSERT INTO users(first_name,last_name) VALUES('$first_name','$last_name')";
  mysql_query($sql_query);
 
         // sql query for inserting data into database
@@ -25,7 +21,7 @@ if(isset($_POST['btn-save']))
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Add User for Admin Dashboard Test</title>
+<title>Add User</title>
 <link rel="stylesheet" href="style.css" type="text/css" />
 </head>
 <body>
@@ -33,7 +29,7 @@ if(isset($_POST['btn-save']))
 
 <div id="header">
  <div id="content">
-    <label>Add User for Admin Dashboard Test</label>
+    <label>Add User</label>
     </div>
 </div>
 <div id="body">
@@ -41,25 +37,13 @@ if(isset($_POST['btn-save']))
     <form method="post">
     <table align="center">
     <tr>
-    <td align="center"><a href="read_data.php">Admin Dashboard</a></td>
-    </tr>
-    <tr>
-    <td><input type="text" name="username" placeholder="Username" required /></td>
+    <td align="center"><a href="index.php">back to main page</a></td>
     </tr>
     <tr>
     <td><input type="text" name="first_name" placeholder="First Name" required /></td>
     </tr>
     <tr>
     <td><input type="text" name="last_name" placeholder="Last Name" required /></td>
-    </tr>
-    <tr>
-    <td><input type="text" name="age" placeholder="22" required /></td>
-    </tr>
-    <tr>
-    <td><input type="text" name="email" placeholder="example@example.com" required /></td>
-    </tr>
-    <tr>
-    <td><input type="text" name="address" placeholder="123 First St Little Rock AK 23434" required /></td>
     </tr>
     <tr>
     <td><button type="submit" name="btn-save"><strong>SAVE</strong></button></td>
