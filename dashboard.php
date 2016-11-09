f3720771f761a9e6c23b290de67e640bb446e4ab
Karl Rohr <kjrohr@fullsail.edu>
2016-11-09 18:14:41 -0500

    Fixed bad sql statement

--- dashboard.php
+++ dashboard.php
@@ -8,7 +8,7 @@
   exit;
  }
  // select loggedin users detail
- $res=mysql_query("SELECT * FROM users WHERE userId=".$_SESSION['user']);
+ $res=mysql_query("SELECT * FROM users WHERE user_id=".$_SESSION['user']);
  $userRow=mysql_fetch_array($res);
  echo $_SESSION['user'];
  echo $userRow['username'];
