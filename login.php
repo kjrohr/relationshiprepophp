1495c9fd7a78178eab64a47f0bef38ac45971cc7
Karl Rohr <kjrohr@fullsail.edu>
2016-11-09 16:52:29 -0500

    testing login system

---
+++ login.php
@@ -0,0 +1,85 @@
+<?php
+ session_start();
+ require_once 'dbconfig.php';
+
+ // it will never let you open index(login) page if session is set
+ if ( isset($_SESSION['user'])!="" ) {
+  header("Location: dashboard.php");
+  exit;
+ }
+
+ $error = false;
+
+ if( isset($_POST['btn-login']) ) {
+
+  // prevent sql injections/ clear user invalid inputs
+  $email = trim($_POST['email']);
+  $email = strip_tags($email);
+  $email = htmlspecialchars($email);
+
+  $pass = trim($_POST['pass']);
+  $pass = strip_tags($pass);
+  $pass = htmlspecialchars($pass);
+  // prevent sql injections / clear user invalid inputs
+
+  if(empty($email)){
+   $error = true;
+   $emailError = "Please enter your email address.";
+  } else if ( !filter_var($email,FILTER_VALIDATE_EMAIL) ) {
+   $error = true;
+   $emailError = "Please enter valid email address.";
+  }
+
+  if(empty($pass)){
+   $error = true;
+   $passError = "Please enter your password.";
+  }
+
+  // if there's no error, continue to login
+  if (!$error) {
+
+   $password = hash('sha256', $pass); // password hashing using SHA256
+
+   $res=mysql_query("SELECT userId, userName, userPass FROM users WHERE userEmail='$email'");
+   $row=mysql_fetch_array($res);
+   $count = mysql_num_rows($res); // if uname/pass correct it returns must be 1 row
+
+   if( $count == 1 && $row['userPass']==$password ) {
+    $_SESSION['user'] = $row['userId'];
+    header("Location: home.php");
+   } else {
+    $errMSG = "Incorrect Credentials, Try again...";
+   }
+
+  }
+
+ }
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Relationship Repo! - Login</title>
+<link rel="stylesheet" href="assets/css/bootstrap.min.css" type="text/css"  />
+<link rel="stylesheet" href="style.css" type="text/css" />
+</head>
+<body>
+  <h2 class="">Sign In.</h2>
+
+
+  <?php
+   if ( isset($errMSG) ) {
+      echo $errMSG;
+    }
+  ?>
+
+  <input type="email" name="email" class="form-control" placeholder="Your Email" value="<?php echo $email; ?>" maxlength="40" />
+  <input type="password" name="pass" class="form-control" placeholder="Your Password" maxlength="15" />
+  <button type="submit" class="btn btn-block btn-primary" name="btn-login">Sign In</button>
+  <a href="add_data.php">Sign Up Here...</a>
+
+    </form>
+
+
+</body>
+</html>
