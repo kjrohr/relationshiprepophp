0521a4b7cab125f7f7acf32bbaf81d6b6bfd5149
Karl Rohr <kjrohr@fullsail.edu>
2016-11-09 18:10:36 -0500

    Misnamed variable

--- dashboard.php
+++ dashboard.php
@@ -10,6 +10,7 @@
  // select loggedin users detail
  $res=mysql_query("SELECT * FROM users WHERE userId=".$_SESSION['user']);
  $userRow=mysql_fetch_array($res);
+ echo $_SESSION['user'];
 ?>
 <!DOCTYPE html>
 <html>
