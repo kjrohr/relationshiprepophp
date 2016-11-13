ab396f7cb69f8e3bc8f42bc71904749c66d2a610
Lindsay Hampton <lrhampton@fullsail.edu>
2016-11-13 17:42:59 -0500

    Styling the login form

--- login.php
+++ login.php
@@ -50,13 +50,6 @@
  }
 ?>
 <!DOCTYPE html>
-<html>
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Relationship Repo! - Login</title>
-<link rel="stylesheet" href="assets/css/bootstrap.min.css" type="text/css"  />
-<link rel="stylesheet" href="style.css" type="text/css" />
-</head>
 <body>
   <div id="body">
    <div id="content">
