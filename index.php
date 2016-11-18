1447930b623326fc7f5e864557c50b6ad108a350
Karl Rohr <kjrohr@fullsail.edu>
2016-11-17 23:23:14 -0500

    Fixed semicolon issues

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
-include 'header.php'
-include 'nav.php'
+include 'header.php';
+include 'nav.php';
 
 ?>
 <!-- NAVBAR
--- login.php
+++ login.php
@@ -1,7 +1,6 @@
 <?php
  session_start();
  include 'header.php';
- include 'nav.php';
  require_once 'dbconfig.php';
 
  // it will never let you open index(login) page if session is set
