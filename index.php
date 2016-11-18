830f11a766b808034724057fed8f51270c194fb7
Lindsay Hampton <lrhampton@fullsail.edu>
2016-11-17 22:34:30 -0500

    Testing fixed footer on index

--- index.php
+++ index.php
@@ -150,7 +150,7 @@ include 'header.php'
 
 
   <!-- FOOTER -->
-  <footer>
+  <footer class="navbar navbar-default navbar-fixed-bottom">
     <p class="pull-right"><a href="#">Back to top</a></p>
     <p>&copy; 2016 Company, Inc. &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
   </footer>
