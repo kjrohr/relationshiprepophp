2eee70c25283a2801962ed30eff52bb0b508192a
Lindsay Hampton <lrhampton@fullsail.edu>
2016-11-17 23:04:00 -0500

    Testing page

--- index.php
+++ index.php
@@ -168,9 +168,10 @@ include 'header.php'
 
 
   <!-- FOOTER -->
-  <footer class="footer">
-    <p><a href="#">Back to top</a></p>
+  <footer>
+    <p class="pull-right"><a href="#">Back to top</a></p>
     <p>&copy; 2016 Company, Inc. &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
+    </div>
   </footer>
 
 </div><!-- /.container -->
