50307d826a6ef590914b2bee334b1027283a4a64
Lindsay Hampton <lrhampton@fullsail.edu>
2016-11-17 22:28:01 -0500

    Testing nav bar on aboutus.php

--- aboutus.php
+++ aboutus.php
@@ -7,4 +7,17 @@ include 'header.php'
 <title>Relationship Repo!</title>
 </head>
 <body>
+
+  <nav class="navbar navbar-default navbar-fixed-top">
+    <div class="container">
+    <a href="/index.php">Home</a>
+    </div>
+    <div class="container">
+    <a href="/aboutus.php">About Us</a>
+    </div>
+    <div class="container">
+    <a href="/packages.php">Packages</a>
+    </div>
+  </nav>
+
 </body>
