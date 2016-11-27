84a1d88fad79d5176c46d22a5ac7768256c19898
Karl Rohr <kjrohr@fullsail.edu>
2016-11-27 13:54:52 -0600

    Changed delete logic again

--- delete_user.php
+++ delete_user.php
@@ -6,9 +6,13 @@
   header("Location: dashboard.php");
  }
 
+$user_id = $_SESSION['user'];
+
  if (isset($_GET['delete'])) {
    // sql to delete a record
-   $res=mysql_query("DELETE FROM users WHERE user_id='$_SESSION['user']'");
+   $res=mysql_query("DELETE FROM users WHERE user_id='$user_id'");
+   $row=mysql_fetch_array($res);
+   $count = mysql_num_rows($res);
 
   unset($_SESSION['user']);
   session_unset();
