b4e142657f85627c4b9cc408c4a4bd66eed2b854
Karl Rohr <kjrohr@fullsail.edu>
2016-11-27 14:01:34 -0600

    Hopefully removed the error

--- delete_user.php
+++ delete_user.php
@@ -9,9 +9,8 @@
 $user_id = $_SESSION['user'];
  if (isset($_GET['delete'])) {
    // sql to delete a record
-   $res=mysql_query("DELETE FROM users WHERE user_id='$user_id'");
-   $row=mysql_fetch_array($res);
-   $count = mysql_num_rows($res);
+   mysql_query("DELETE FROM users WHERE user_id='$user_id'");
+
 
   unset($_SESSION['user']);
   session_unset();
