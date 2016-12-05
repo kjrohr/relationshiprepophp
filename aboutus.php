edceca7042e4d7cfb02c0acbded4776df872deef
Lindsay Hampton <lrhampton@fullsail.edu>
2016-12-05 14:04:23 -0500

    Testing live chat feature

--- aboutus.php
+++ aboutus.php
@@ -25,5 +25,18 @@ include 'nav.php';
     </div>
   </footer>
 
+  <!--Start of Tawk.to Script-->
+  <script type="text/javascript">
+  var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+  (function(){
+  var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+  s1.async=true;
+  s1.src='https://embed.tawk.to/5845b8372d02a274f9b74057/default';
+  s1.charset='UTF-8';
+  s1.setAttribute('crossorigin','*');
+  s0.parentNode.insertBefore(s1,s0);
+  })();
+  </script>
+  <!--End of Tawk.to Script-->
 
 </body>
--- index.php
+++ index.php
@@ -166,11 +166,19 @@ include 'nav.php';
           </div>
       </footer>
 
-<? php
-
-include 'livechat.php'
-
-?>
+      <!--Start of Tawk.to Script-->
+      <script type="text/javascript">
+      var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+      (function(){
+      var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+      s1.async=true;
+      s1.src='https://embed.tawk.to/5845b8372d02a274f9b74057/default';
+      s1.charset='UTF-8';
+      s1.setAttribute('crossorigin','*');
+      s0.parentNode.insertBefore(s1,s0);
+      })();
+      </script>
+      <!--End of Tawk.to Script-->
 
 </body>
 </html>
