cf3505d43bdbb4918355ad02e17cd318f24f53f9
Lindsay Hampton <lrhampton@fullsail.edu>
2016-12-07 01:54:04 -0500

    testing on login page

--- login.php
+++ login.php
@@ -62,6 +62,50 @@
 ?>
 <!DOCTYPE html>
 <body>
+
+  <header>
+      <div class="container">
+
+          <div class="row">
+              <div class="col-sm-7">
+                  <div class="header-content">
+                      <div class="header-content-inner">
+                          <h1 style="margin-bottom:0; font-size: 57px;">Sometimes, Things Just Don't Work Out</h1>
+                          <h2 style="margin-top:0; margin-bottom:30px; font-size:30px;">Relationship Repo is here for you - through the good times, and the bad.</h2>
+                          <a href="#signup-modal" data-toggle="modal" class="btn btn-outline btn-xl page-scroll">Sign Up Today</a>
+
+
+
+
+
+
+
+
+
+
+                      </div>
+                  </div>
+              </div>
+              <div class="col-sm-5">
+                  <div class="device-container">
+                      <div class="device-mockup iphone6_plus portrait white">
+                          <div class="device">
+                              <div class="screen">
+                                  <!-- Demo image for screen mockup, you can put an image here, some HTML, an animation, video, or anything else! -->
+                                  <img src="img/demo-screen-1.jpg" class="img-responsive" alt="">
+                              </div>
+                              <div class="button">
+                                  <!-- You can hook the "home button" to some JavaScript events or just remove it -->
+                              </div>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+          </div>
+      </div>
+  </header>
+
+  
   <div id="body">
    <div id="content">
 
