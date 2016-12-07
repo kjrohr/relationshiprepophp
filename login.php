846c20e24fd01ce96a3e28d627003c87010d2e71
Lindsay Hampton <lrhampton@fullsail.edu>
2016-12-07 01:56:00 -0500

    testing on login page

--- login.php
+++ login.php
@@ -64,48 +64,67 @@
 <body>
 
   <header>
-      <div class="container">
 
-          <div class="row">
-              <div class="col-sm-7">
-                  <div class="header-content">
-                      <div class="header-content-inner">
-                          <h1 style="margin-bottom:0; font-size: 57px;">Sometimes, Things Just Don't Work Out</h1>
-                          <h2 style="margin-top:0; margin-bottom:30px; font-size:30px;">Relationship Repo is here for you - through the good times, and the bad.</h2>
-                          <a href="#signup-modal" data-toggle="modal" class="btn btn-outline btn-xl page-scroll">Sign Up Today</a>
+        <div class="container" style="max-width:530px; margin: 0 auto;">
+                    <form class="form-horizontal" role="form">
+                        <h2>Registration Form</h2>
+                        <div style="margin-left: 30%;">
+                        <div class="form-group">
+                          <div class="col-sm-9">
+                                <input type="text" name="username" placeholder="Username" class="form-control" required />
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <div class="col-sm-9">
+                                <input type="text" name="first_name" placeholder="First Name" class="form-control" required />
+                            </div>
+                        </div>
+                        <div class="form-group">
+                          <div class="col-sm-9">
+                                <input type="text" name="last_name" placeholder="Last Name" class="form-control" required />
+                            </div>
+                        </div>
+                        <div class="form-group">
+                          <div class="col-sm-9">
+                                <input type="email" name="email" placeholder="Email Address" class="form-control" required />
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <div class="col-sm-9">
+                                <input type="text" name="address" placeholder="Address, City, State, Zip" class="form-control" required />
+                            </div>
+                        </div> <!-- /.form-group -->
+                        <div class="form-group">
+                          <div class="col-sm-9">
+                            <input type="number" name="age" placeholder="Your Age" class="form-control" required />
+                            </div>
+                        </div> <!-- /.form-group -->
+                        <div class="form-group">
+                          <div class="col-sm-9">
+                            <input type="password" name="pass" placeholder="Your Password" class="form-control" required />
+                            </div>
+                        </div> <!-- /.form-group -->
+                        <div class="form-group">
+                            <div class="col-sm-9">
+                                <div class="checkbox">
+                                    <label>
+                                        <input type="checkbox">By checking this box, I hearby accept Relationship Repo's <a href="#">Terms and Conditions</a>.
+                                    </label>
+                                </div>
+                            </div>
+                        </div> <!-- /.form-group -->
+                        <div class="form-group">
+                            <div class="col-sm-9">
+                                <button type="submit" name="btn-save" class="btn btn-primary btn-block">Register</button>
+                            </div>
+                        </div>
+                        </div>
+                    </form> <!-- /form -->
+                </div> <!-- ./container -->
 
-
-
-
-
-
-
-
-
-
-                      </div>
-                  </div>
-              </div>
-              <div class="col-sm-5">
-                  <div class="device-container">
-                      <div class="device-mockup iphone6_plus portrait white">
-                          <div class="device">
-                              <div class="screen">
-                                  <!-- Demo image for screen mockup, you can put an image here, some HTML, an animation, video, or anything else! -->
-                                  <img src="img/demo-screen-1.jpg" class="img-responsive" alt="">
-                              </div>
-                              <div class="button">
-                                  <!-- You can hook the "home button" to some JavaScript events or just remove it -->
-                              </div>
-                          </div>
-                      </div>
-                  </div>
-              </div>
-          </div>
-      </div>
   </header>
 
-  
+
   <div id="body">
    <div id="content">
 
