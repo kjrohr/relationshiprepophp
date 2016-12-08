8982ec8234711e157efb0a2753c6b45a6920acd7
Lindsay Hampton <lrhampton@fullsail.edu>
2016-12-08 10:04:07 -0500

    Trying to do a fade thingy with the logo

--- nav.php
+++ nav.php
@@ -39,8 +39,9 @@
 
 $( window ).scroll(function() {
      if($(window).scrollTop() > 150){
-       $('#pagelogo').attr("src","img/logo3.png");
-     } else{
+       $('#pagelogo').hide();
+       $('#pagelogo').attr("src","img/logo3.png").fadeIn();
+     } else if ($(window).scrollTop() <= 150){
          $('#pagelogo').attr("src","img/logo2.png");
      }
 
