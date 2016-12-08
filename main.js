583eb6f83fa0b85e421d58379f036b2f9ecd5b97
Karl Rohr <kjrohr@fullsail.edu>
2016-12-08 18:16:13 -0500

    Starting with ajax to move data from jquery to php

--- main.js
+++ main.js
@@ -1,7 +1,19 @@
 $(document).ready(function(){
+  // $('.claim').on('click', function(){
+  //   var x = $(this).parent().parent().children(':first-child').text();
+  //   console.log(x);
+  // });
+
   $('.claim').on('click', function(){
+
     var x = $(this).parent().parent().children(':first-child').text();
-    console.log(x);
+    $.ajax({
+                url: 'dashboard.php',
+                dataType: 'text',
+                type: 'post',
+                contentType: 'application/x-www-form-urlencoded',
+                data: x
+            });
   });
 
 });
