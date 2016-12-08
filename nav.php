8c657e4c241fbf48478e30a9fb4105fee471fecb
Lindsay Hampton <lrhampton@fullsail.edu>
2016-12-08 07:28:53 -0500

    Maybe jquery to resize logo?

--- nav.php
+++ nav.php
@@ -9,8 +9,18 @@
             <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
                 <span class="sr-only">Toggle navigation</span> Menu <i class="fa fa-bars"></i>
             </button>
-            <a class="navbar-brand page-scroll" style="padding-top:5px;" href="#page-top"><img src="img/logo2.png" alt="Relationship Repo"/></a>
+            <a class="navbar-brand page-scroll" style="padding-top:5px;" href="#page-top"><img src="img/logo2.png" id="pagelogo" alt="Relationship Repo"/></a>
         </div>
+
+        <script> <!-- jQuery script to resize the nav logo -->
+
+        $( window ).scroll(function() {
+             if($(window).scrollTop() > 200){
+               $('#pagelogo').css({'height': '50'});
+             }
+        });
+
+        </script>
 
         <!-- Collect the nav links, forms, and other content for toggling -->
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
