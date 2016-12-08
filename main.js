6adebd41db513cbe44246f495ffe391bf2ebf794
Karl Rohr <kjrohr@fullsail.edu>
2016-12-08 18:22:27 -0500

    Rewrote ajax call

--- main.js
+++ main.js
@@ -8,19 +8,13 @@ $(document).ready(function(){
 
     var x = $(this).parent().parent().children(':first-child').text();
     $.ajax({
-                url: 'dashboard.php',
-                dataType: 'text',
-                type: 'post',
-                contentType: 'application/x-www-form-urlencoded',
-                data: $(this).serialize(),
-                success: function( data, textStatus, jQxhr ){
-                    //$('#response pre').html( data );
-                    console.log(data);
-                },
-                error: function( jqXhr, textStatus, errorThrown ){
-                    console.log( errorThrown );
-                }
-            });
+      method: "POST",
+      url: "dashboard.php",
+      data: x
+    })
+      .done(function( msg ) {
+        alert( "Data Saved: " + msg );
+      });
   });
 
 });
