ea15eadfdaa2f796208e1ef71641af06b99d7571
Karl Rohr <kjrohr@fullsail.edu>
2016-12-09 14:53:37 -0500

    Beginning to style the dashboards

--- dashboard.php
+++ dashboard.php
@@ -50,8 +50,6 @@
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title>Welcome - <?php echo $userRow['username']; ?></title>
-<link rel="stylesheet" href="assets/css/bootstrap.min.css" type="text/css"  />
-<link rel="stylesheet" href="style.css" type="text/css" />
 </head>
 <body>
 Hello <?php echo $userRow['username']; ?>
