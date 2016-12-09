cfb28df410ad2ef569efc7cdcb70ebd2f13edd6b
Lindsay Hampton <lrhampton@fullsail.edu>
2016-12-08 20:58:47 -0500

    WHY

--- agents.php
+++ agents.php
@@ -10,15 +10,15 @@ include 'nav.php';
 
   <body id="page-top">
 
-      <header>
+      <header style="background:url('img/laugh.png')">
           <div class="container">
 
               <div class="row">
                   <div class="col-sm-7">
                       <div class="header-content">
                           <div class="header-content-inner">
-                              <h1 style="margin-bottom:0; font-size: 57px;">Sometimes, Things Just Don't Work Out</h1>
-                              <h2 style="margin-top:0; margin-bottom:30px; font-size:30px;">Relationship Repo is here for you - through the good times, and the bad.</h2>
+                              <h1 style="margin-bottom:0; font-size: 57px;">So You Want To Become An Agent With Us?</h1>
+                              <h2 style="margin-top:0; margin-bottom:30px; font-size:30px;">Relationship Repo takes our agent roles very seriously.</h2>
                               <a href="#signup-modal" data-toggle="modal" class="btn btn-outline btn-xl page-scroll">Sign Up Today</a>
                               <a href="/login.php" class="btn btn-outline btn-xl page-scroll" style="margin-left:25px">Account Login</a>
 
