a18d2f3faa849c774b77f228fe323ee9f4c06c7c
Karl Rohr <kjrohr@fullsail.edu>
2016-12-12 19:39:42 -0500

    Restarting stripe completely

--- stripe_test.php
+++ stripe_test.php
@@ -1,47 +1,35 @@
 <?php
- include 'header.php';
+include "header.php";
+?>
 
- if (isset($_POST['stripeToken'])) {
-   echo 'token exists';
- }
+</head>
+<body>
+<form action="/your-charge-code" method="POST" id="payment-form">
+  <span class="payment-errors"></span>
 
- ?>
- </head>
- <body>
-   <form method="POST" id="payment-form">
-    <span class="payment-errors"></span>
+  <div class="form-row">
+    <label>
+      <span>Card Number</span>
+      <input type="text" size="20" data-stripe="number">
+    </label>
+  </div>
 
-    <div class="form-row">
-      <label>
-        <span>Card Number</span>
-        <input type="text" size="20" data-stripe="number">
-      </label>
-    </div>
+  <div class="form-row">
+    <label>
+      <span>Expiration (MM/YY)</span>
+      <input type="text" size="2" data-stripe="exp_month">
+    </label>
+    <span> / </span>
+    <input type="text" size="2" data-stripe="exp_year">
+  </div>
 
-    <div class="form-row">
-      <label>
-        <span>Expiration (MM/YY)</span>
-        <input type="text" size="2" data-stripe="exp_month">
-      </label>
-      <span> / </span>
-      <input type="text" size="2" data-stripe="exp_year">
-    </div>
-
-    <div class="form-row">
-      <label>
-        <span>CVC</span>
-        <input type="text" size="4" data-stripe="cvc">
-      </label>
-    </div>
+  <div class="form-row">
+    <label>
+      <span>CVC</span>
+      <input type="text" size="4" data-stripe="cvc">
+    </label>
+  </div>
 
 
-    <input type="submit" class="submit" value="Submit Payment">
-  </form>
-
- <script type="text/javascript" src="https://js.stripe.com/v2/"></script>
- <script type="text/javascript">
-  Stripe.setPublishableKey('pk_test_mVRIEcTc4r6RkVTj74wurQFE');
- </script>
- <script src="main.js"></script>
-</body>
-</html>
+  <input type="submit" class="submit" value="Submit Payment">
+</form>
