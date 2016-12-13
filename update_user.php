ce64e11cbab520f44e27deb0a9310c277e896f5a
Karl Rohr <kjrohr@fullsail.edu>
2016-12-13 11:29:53 -0500

    Hopefully added nav to update user

--- update_user.php
+++ update_user.php
@@ -40,6 +40,39 @@ if(isset($_POST['btn-save']))
 <title>Update User</title>
 </head>
 <body>
+  <nav id="mainNav" class="navbar navbar-default navbar-fixed-top">
+      <div class="container">
+          <!-- Brand and toggle get grouped for better mobile display -->
+          <div class="navbar-header">
+              <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                  <span class="sr-only">Toggle navigation</span> Menu <i class="fa fa-bars"></i>
+              </button>
+              <a class="navbar-brand page-scroll" style="padding-top:5px;" href="#page-top">
+                <img src="img/logo3.png" id="pagelogo" alt="Relationship Repo"/>
+                </a>
+          </div>
+
+          <!-- Collect the nav links, forms, and other content for toggling -->
+          <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+              <ul class="nav navbar-nav navbar-right">
+                <li>
+                    <a href="/index.php">Hello <?php echo $userRow['username']; ?></a>
+                </li>
+                  <li>
+                      <a href="/aboutus.php">About Us</a>
+                  </li>
+                  <li>
+                      <a href="update_user.php">Update Info</a>
+                  </li>
+                  <li>
+                      <a href="logout.php?logout"><strong>Logout</strong></a>
+                  </li>
+              </ul>
+          </div>
+          <!-- /.navbar-collapse -->
+      </div>
+      <!-- /.container-fluid -->
+  </nav>
 <center>
 
 <div id="header">
