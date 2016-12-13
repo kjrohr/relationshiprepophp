fcb44b8cc11abd3c226ff69654dd32b3f2413efe
Karl Rohr <kjrohr@fullsail.edu>
2016-12-12 19:27:04 -0500

    Replaced form for testing

--- stripe_test.php
+++ stripe_test.php
@@ -10,36 +10,35 @@
  ?>
  </head>
  <body>
- <form method="POST" id="payment-form">
-   <span class="payment-errors"></span>
+   <form method="POST" id="payment-form">
+    <span class="payment-errors"></span>
 
-   <div class="form-row">
-     <label>
-       <span>Card Number</span>
-       <input type="text" size="20" data-stripe="number">
-     </label>
-   </div>
+    <div class="form-row">
+      <label>
+        <span>Card Number</span>
+        <input type="text" size="20" data-stripe="number">
+      </label>
+    </div>
 
-   <div class="form-row">
-     <label>
-       <span>Expiration (MM/YY)</span>
-       <input type="text" size="2" data-stripe="exp_month">
-     </label>
-     <span> / </span>
-     <input type="text" size="2" data-stripe="exp_year">
-   </div>
+    <div class="form-row">
+      <label>
+        <span>Expiration (MM/YY)</span>
+        <input type="text" size="2" data-stripe="exp_month">
+      </label>
+      <span> / </span>
+      <input type="text" size="2" data-stripe="exp_year">
+    </div>
 
-   <div class="form-row">
-     <label>
-       <span>CVC</span>
-       <input type="text" size="4" data-stripe="cvc">
-     </label>
-   </div>
+    <div class="form-row">
+      <label>
+        <span>CVC</span>
+        <input type="text" size="4" data-stripe="cvc">
+      </label>
+    </div>
 
 
-   <input type="submit" class="submit" value="Submit Payment">
- </form>
-
+    <input type="submit" class="submit" value="Submit Payment">
+  </form>
 
  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
  <script type="text/javascript" src="https://js.stripe.com/v2/"></script>
