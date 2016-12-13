23597ec194212382aafe6f3d24b32b15d0b2e413
Karl Rohr <kjrohr@fullsail.edu>
2016-12-13 11:41:03 -0500

    Added footer

--- update_user.php
+++ update_user.php
@@ -137,4 +137,20 @@ if(isset($_POST['btn-save']))
 </div>
 </section>
 </div>
+<footer>
+    <div class="container">
+        <p>&copy; 2016 Relationship Repo. All Rights Reserved.</p>
+        <ul class="list-inline">
+            <li>
+                <a href="#">Privacy</a>
+            </li>
+            <li>
+                <a href="#">Terms</a>
+            </li>
+            <li>
+                <a href="#">FAQ</a>
+            </li>
+        </ul>
+    </div>
+</footer>
 </body>
