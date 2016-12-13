e4829df8b8f14ca290a0df5d38e47f3df749859a
Karl Rohr <kjrohr@fullsail.edu>
2016-12-13 08:41:02 -0500

    Hoping I made the right changes

--- dashboard.php
+++ dashboard.php
@@ -54,21 +54,35 @@
 </head>
 <body>
   <nav id="mainNav" class="navbar navbar-default navbar-fixed-top">
-  <!-- Collect the nav links, forms, and other content for toggling -->
-  <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-      <ul class="nav navbar-nav navbar-right">
-        <li>
-            <a href="/index.php">Home</a>
-        </li>
-          <li>
-              <a href="/aboutus.php">About Us</a>
-          </li>
-          <li>
-              <a href="/login.php"><strong>Login</strong></a>
-          </li>
-      </ul>
-  </div>
-</nav>
+      <div class="container">
+          <!-- Brand and toggle get grouped for better mobile display -->
+          <div class="navbar-header">
+              <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                  <span class="sr-only">Toggle navigation</span> Menu <i class="fa fa-bars"></i>
+              </button>
+              <a class="navbar-brand page-scroll" style="padding-top:5px;" href="#page-top">
+                <img src="img/logo3.png" id="pagelogo" alt="Relationship Repo"/>
+                </a>
+          </div>
+
+          <!-- Collect the nav links, forms, and other content for toggling -->
+          <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+              <ul class="nav navbar-nav navbar-right">
+                <li>
+                    <a href="/index.php">Home</a>
+                </li>
+                  <li>
+                      <a href="/aboutus.php">About Us</a>
+                  </li>
+                  <li>
+                      <a href="/login.php"><strong>Login</strong></a>
+                  </li>
+              </ul>
+          </div>
+          <!-- /.navbar-collapse -->
+      </div>
+      <!-- /.container-fluid -->
+  </nav>
 <div class='container'>
   <div class='row'>
     <div class="col-sm-7">
