089e6f780670ea195f05aa78bcc69e5eaf926ee1
Karl Rohr <kjrohr@fullsail.edu>
2016-12-12 20:19:37 -0500

    Making sure the jquery is working

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+  alert('I AM WORKING!');
   $('.claim').on('click', function(){
     var case_id = $(this).parent().parent().children(':first-child').text();
     $.post('dashboard.php', 'case_id=' + case_id, function (response) {
