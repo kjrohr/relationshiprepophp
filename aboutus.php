49f43fce2146c6238577144ed7311f126aab0d43
Lindsay Hampton <lrhampton@fullsail.edu>
2016-12-14 12:46:38 -0500

    Changing About Us page

--- aboutus.php
+++ aboutus.php
@@ -8,20 +8,33 @@ include 'nav.php';
 <title>Relationship Repo!</title>
 </head>
 <body id="page-top">
-<header style="background:url('img/sun.jpg') no-repeat center center fixed; background-size: cover;">
 
-  <!-- Begin page content -->
-    <div class="page-header">
-      <h1 style="margin:25% 0 0; font-size: 57px;">Who Are We?</h1>
-    </div>
+  <header>
+      <div class="container">
+
+          <div class="row">
+              <div class="col-sm-7">
+                  <div class="header-content">
+                      <div class="header-content-inner">
+                          <h1 style="margin-bottom:0; font-size: 57px;">What is Relationship Repo?</h1>
+                          <h2 style="margin-top:0; margin-bottom:30px; font-size:30px;">Relationship Repo is here for you - through the good times, and the bad.</h2>
+                      </div>
+                  </div>
+              </div>
+          </div>
+      </div>
   </header>
 
-  <div class="container" style="background: #000">
-      <h2>Sometimes, Things Just Don't Work Out</h2>
-      <p>Relationship Repo is an idea that was created by Lindsay Hampton and Karl Rohr. We at Relationship Repo want to take the pain out of having a bad break up.</p>
-      <p>Sometimes, this don't go as planned and you need to leave the situation that you are in and sometimes that means leaving behind a lot of valuable belongings.</p>
-      <p>We want to help you reclaim those items safely and professionally. You can submit a 'case' to our Agents and one will select it and contact you shortly.</p>
-  </div>
+  <section class="cta">
+      <div class="cta-content">
+          <div class="container">
+            <h1 style="margin-bottom:0; font-size: 57px;">Meet Our Team</h1>
+            <h2 style="margin-top:0; margin-bottom:30px; font-size:30px; color: #333;">We are always looking for new agents to join our team!</h2>
+          </div>
+      </div>
+      <div class="overlay"></div>
+  </section>
+
 
   <footer>
       <div class="container">
