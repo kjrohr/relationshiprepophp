161fb38dd0e04b6dc3ed4f42fedf83fd878bb062
Lindsay Hampton <lrhampton@fullsail.edu>
2016-12-14 16:10:34 -0500

    Adding more to Index so it's not so empty

--- index.php
+++ index.php
@@ -61,25 +61,25 @@ if(isset($_POST['btn-save']))
       <section style="background: #A26F6E; padding:90px 0;" class="download bg-primary text-center">
           <div class="container">
               <div class="row">
-                  <div class="col-md-10 col-md-offset-1" style="margin-bottom:45px;">
+                  <div class="col-md-10 col-md-offset-1" style="margin-bottom:50px;">
                       <h2 class="section-heading" style="font-size: 47px; margin-bottom:0;">What Can Relationship Repo Do For You?</h2>
                   </div>
                   <div class="container-fluid text-center">
                       <div class="row">
                         <div class="col-sm-4">
                           <span class="glyphicon glyphicon-user" style="font-size:60px;"></span>
-                          <h4 style="font-size:30px;">Resources</h4>
-                          <p>While working with Relationship Repo, you're working with <strong>real people</strong> all of the time. No bots, no scams, no tricks. Choose your own agent or send us a live chat message any time you need help.</p>
+                          <h4 style="font-size:40px;">Resources</h4>
+                          <p style="font-size:13px;">While working with Relationship Repo, you're working with <strong>real people</strong> all of the time. No bots, no scams, no tricks. Choose your own agent or send us a live chat message any time you need help.</p>
                         </div>
                         <div class="col-sm-4">
                           <span class="glyphicon glyphicon-heart" style="font-size:60px;"></span>
-                          <h4 style="font-size:30px;">Compassion</h4>
-                          <p>Realistically, we've all had a time where we've been in a similar situation to yours. We understand what you're going through and we're here to help.</p>
+                          <h4 style="font-size:40px;">Compassion</h4>
+                          <p style="font-size:13px;">Realistically, we've all had a time where we've been in a similar situation to yours. We understand what you're going through and we're here to help.</p>
                         </div>
                         <div class="col-sm-4">
                           <span class="glyphicon glyphicon-thumbs-up" style="font-size:60px;"></span>
-                          <h4 style="font-size:30px;">Satisfaction</h4>
-                          <p>Our systems are set up to allow you to choose which agent you'd like to take on your case. Once you've connected with your agent, you work together to form your own contract that suits your needs.</p>
+                          <h4 style="font-size:40px;">Satisfaction</h4>
+                          <p style="font-size:13px;">Our systems are set up to allow you to choose which agent you'd like to take on your case. Once you've connected with your agent, you work together to form your own contract that suits your needs.</p>
                         </div>
                         </div>
                   </div>
