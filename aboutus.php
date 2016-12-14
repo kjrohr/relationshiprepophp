2b7d6149160a58a39b52266b00be34250958d241
Lindsay Hampton <lrhampton@fullsail.edu>
2016-12-14 13:25:19 -0500

    Changing About Us page

--- aboutus.php
+++ aboutus.php
@@ -35,21 +35,21 @@ include 'nav.php';
   <section class="cta">
       <div class="cta-content">
           <div class="container">
-            <p>
+            <h2>
               Relationship Repo is a web service for any person who has lost their possessions to a relationship beyond repair. We are a company dedicated to connecting clients with with third-party sources who specialize in the delicate nature of recently ended relationships. Unlike any other company on the market today, Relationship Repo offers a unique and sought after service that
                is both practical and comforting to those who may pursue further action.
-            </p>
+            </h2>
           </div>
       </div>
       <div class="overlay" style="background: transparent;"></div>
   </section>
 
-  <section class="cta">
+  <section class="cta" style="padding:0;">
       <div class="cta-content">
           <div class="container">
             <h3>Remember that girlfriend back in college who stole your heart and your favorite hoodie? There's still hope for the hoodie, and all the others out there like it.</h2>
       </div>
-      <div class="overlay"></div>
+      <div class="overlay" style="padding:0;"></div>
   </section>
 
   <section class="cta">
