fec4952336b673d673e02e8ab72b6b5d9a11c3c6
Lindsay Hampton <lrhampton@fullsail.edu>
2016-12-14 15:36:33 -0500

    Adding more to Index so it's not so empty

--- index.php
+++ index.php
@@ -61,24 +61,24 @@ if(isset($_POST['btn-save']))
       <section style="background: #A26F6E; padding:90px 0;" class="download bg-primary text-center">
           <div class="container">
               <div class="row">
-                  <div class="col-md-10 col-md-offset-1">
+                  <div class="col-md-10 col-md-offset-1" style="margin-bottom:15px;">
                       <h2 class="section-heading" style="font-size: 47px; margin-bottom:0;">What Can Relationship Repo Do For You?</h2>
                   </div>
                   <div class="container-fluid text-center">
                       <div class="row">
                         <div class="col-sm-4">
                           <span class="glyphicon glyphicon-off"></span>
-                          <h4>POWER</h4>
+                          <h4 style="font-size:30px;">POWER</h4>
                           <p>Lorem ipsum dolor sit amet..</p>
                         </div>
                         <div class="col-sm-4">
                           <span class="glyphicon glyphicon-heart"></span>
-                          <h4>LOVE</h4>
+                          <h4 style="font-size:30px;">LOVE</h4>
                           <p>Lorem ipsum dolor sit amet..</p>
                         </div>
                         <div class="col-sm-4">
                           <span class="glyphicon glyphicon-lock"></span>
-                          <h4>JOB DONE</h4>
+                          <h4 style="font-size:30px;">JOB DONE</h4>
                           <p>Lorem ipsum dolor sit amet..</p>
                         </div>
                         </div>
@@ -86,17 +86,17 @@ if(isset($_POST['btn-save']))
                       <div class="row">
                         <div class="col-sm-4">
                           <span class="glyphicon glyphicon-leaf"></span>
-                          <h4>GREEN</h4>
+                          <h4 style="font-size:30px;">GREEN</h4>
                           <p>Lorem ipsum dolor sit amet..</p>
                         </div>
                         <div class="col-sm-4">
                           <span class="glyphicon glyphicon-certificate"></span>
-                          <h4>CERTIFIED</h4>
+                          <h4 style="font-size:30px;">CERTIFIED</h4>
                           <p>Lorem ipsum dolor sit amet..</p>
                         </div>
                         <div class="col-sm-4">
                           <span class="glyphicon glyphicon-wrench"></span>
-                          <h4>HARD WORK</h4>
+                          <h4 style="font-size:30px;">HARD WORK</h4>
                           <p>Lorem ipsum dolor sit amet..</p>
                         </div>
                     </div>
